f0f9d3955b803a85bf964ed086e68237d720d5e5
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-11 16:44:08 +0900

    feat: 데이터 변경

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,39 @@
-export const people = [
+export let movies = [
   {
-    id: '0',
-    name: 'Nicolas',
-    age: 18,
-    gender: 'female',
+    id: 0,
+    name: 'Star Wars = The new one',
+    score: 1,
   },
   {
-    id: '1',
-    name: 'Tom',
-    age: 28,
-    gender: 'male',
+    id: 1,
+    name: 'Avengers - The new one',
+    score: 8,
   },
   {
-    id: '2',
-    name: 'Anne',
-    age: 24,
-    gender: 'female',
+    id: 2,
+    name: 'The Godfather I',
+    score: 99,
   },
   {
-    id: '3',
-    name: 'Henry',
-    age: 25,
-    gender: 'male',
-  },
-  {
-    id: '4',
-    name: 'Bob',
-    age: 29,
-    gender: 'male',
-  },
-  {
-    id: '5',
-    name: 'Carry',
-    age: 28,
-    gender: 'female',
+    id: 3,
+    name: 'Logan',
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => String(id) === person.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
