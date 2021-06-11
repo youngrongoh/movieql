8208e09160578d98254993980b6038b62c9a9580
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-11 17:41:22 +0900

    feat: 데이터 추가, 삭제 요청 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,13 @@
-import { getMovies, getById } from './db';
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
     movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
